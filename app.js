71f4ace913e66e945030cb002604259be07541a2
Christina Cornell <cdhorn515@gmail.com>
2017-06-20 15:50:13 -0400

    images and info rendering to page, need to style

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 const express = require('express');
-const data = require('data.js');
+const mustacheExpress = require('mustache-express');
+//this constant renames the object module.exports in the data.js file
+const data = require('./data.js');
 const app = express();
+
+//set up template named mustache
+app.engine('mustache', mustacheExpress());
+
+//view engine is set to engin that was named above
+app.set('view engine', 'mustache');
+
+//telling express where to view our files--they are in the views folder
+app.set('views', './views');
+
+//name of the view wanted to load--url will have /directory
+app.get('/directory', function (req, res) {
+  var context = data;
+
+  res.render('directory', context);
+
+});
+
+app.listen(3000, function(){
+  console.log('successsfully started express app!');
+});
